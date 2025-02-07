180c8281462e405709d714fbffd8a0ee1b0dd298
aimee-annabelle <inezannabelle@gmail.com>
2025-02-07 16:16:18 +0200

    main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,143 @@
 import 'package:flutter/material.dart';
+import 'screens/pixel_screen.dart';
+import 'screens/laptop_screen.dart';
+import 'screens/tablet_screen.dart';
+import 'screens/pendrive_screen.dart';
+import 'screens/floppy_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Product Navigation',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: ProductNavigation(),
+      routes: {
+        '/pixel': (context) => PixelScreen(),
+        '/laptop': (context) => LaptopScreen(),
+        '/tablet': (context) => TabletScreen(),
+        '/pendrive': (context) => PendriveScreen(),
+        '/floppy': (context) => FloppyScreen(),
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class ProductNavigation extends StatelessWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Product Navigation'),
+      ),
+      body: ListView(
+        children: [
+          ProductCard(
+            title: 'pixel 1',
+            description: 'Pixel is the most featureful phone ever',
+            price: 800,
+            color: Colors.blue,
+            route: '/pixel',
+          ),
+          ProductCard(
+            title: 'laptop',
+            description: 'Laptop is most productive development tool',
+            price: 2000,
+            color: Colors.green,
+            route: '/laptop',
+          ),
+          ProductCard(
+            title: 'tablet',
+            description: 'Tablet is the most useful device for meeting',
+            price: 1500,
+            color: Colors.amber,
+            route: '/tablet',
+          ),
+          ProductCard(
+            title: 'pen drive',
+            description: 'iPhone is the stylist phone ever',
+            price: 100,
+            color: Colors.deepOrange,
+            route: '/pendrive',
+          ),
+          ProductCard(
+            title: 'Floppy Drive',
+            description: 'Floppy is the stylist phone ever',
+            price: 100,
+            color: Colors.teal,
+            route: '/floppy',
+          ),
+        ],
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class ProductCard extends StatelessWidget {
+  final String title;
+  final String description;
+  final double price;
+  final Color color;
+  final String route;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  const ProductCard({
+    required this.title,
+    required this.description,
+    required this.price,
+    required this.color,
+    required this.route,
+  });
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return Card(
+      margin: EdgeInsets.all(8.0),
+      child: InkWell(
+        onTap: () => Navigator.pushNamed(context, route),
+        child: Row(
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              color: color,
+              alignment: Alignment.center,
+              child: Text(
+                title,
+                style: TextStyle(color: Colors.white),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      title.toUpperCase(),
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text(description),
+                    Text('Price: \$${price.toStringAsFixed(0)}'),
+                    Row(
+                      children: [
+                        Icon(Icons.star_border, color: Colors.red),
+                        Icon(Icons.star_border, color: Colors.red),
+                        Icon(Icons.star_border, color: Colors.red),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
